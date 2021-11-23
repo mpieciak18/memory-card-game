70a04e3fb8e12c57ea59dd6ea44470c158c3f99b
Mark Pieciak <mpieciak18@msn.com>
2021-11-23 12:34:52 -0500

    render 3 random dogs upon loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import Game from '../components/Game.js'
 import { useState, useEffect } from 'react'
-import './App.css'
+// import './App.css'
+import bichonFrise from '../images/bichon-frise.png'
+import borderTerrier from '../images/border-terrier.png'
+import cavSpaniel from '../images/cavalier-king-charles-spaniel.png'
+import chihuahua from '../images/chihuahua.png'
+import daschund from '../images/daschund.png'
+import frenchie from '../images/french-bulldog.png'
+import jackRussell from '../images/jack-russell-terrier.png'
+import miniSchnauzer from '../images/miniature-schnauzer.png'
+import pomeranian from '../images/pomeranian.png'
+import poodle from '../images/poodle.png'
+import pug from '../images/pug.png'
+import shihTzu from '../images/shih-tzu.png'
+import welshCorgi from '../images/welsh-corgi.png'
+import westie from '../images/west-highland-white-terrier.png'
+import yorkie from '../images/yorkshire-terrier.png'
 
 const App = () => {
   const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(false)
 
-  const [dogs, setDogs] = useState({})
+  // Initialize 'dogs' state
+  const dogList = [bichonFrise, borderTerrier, cavSpaniel, chihuahua, daschund, frenchie, jackRussell,
+    miniSchnauzer, pomeranian, poodle, pug, shihTzu, welshCorgi, welshCorgi, westie, yorkie
+  ]
+
+  let initDogState = [];
+  for (let i = 0; i < dogList.length; i++) {
+    const newDogObj = {
+        clicks: 0,
+        id: i,
+        source: dogList[i]
+    }
+    initDogState = [...initDogState, newDogObj]
+  }
+
+  const [dogs, setDogs] = useState(initDogState)
 
   // Use useEffect to check if game is over
   useEffect(() => {
@@ -18,10 +50,10 @@ const App = () => {
 
   return (
     <div id="App">
-      <Header score={score} />
-      <Game dogs={dogs} setDogs={setDogs} setScore={setScore} />
+      {/* <Header score={score} /> */}
+      <Game dogs={dogs} setDogs={setDogs} setScore={setScore} setGameOver={setGameOver}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 const Game = (props) => {
-    const { dogs, setDogs, setScore } = props
+    const { dogs, setDogs, setScore, setGameOver } = props
 
     const clickDog = (e) => {
         const dogId = e.target.id
@@ -11,4 +11,65 @@ const Game = (props) => {
     
         // Insert code to call function to rerender with random cards
     }
+
+    let { idOne, srcOne, idTwo, srcTwo, idThree, srcThree} = ''
+
+    const chooseThreeNums = () => {
+        let { numOne, numTwo, numThree } = ''
+        numOne = Math.floor(Math.random() * 15)
+
+        const chooseNumTwo = () => {
+            let num = '';
+            while (num == numOne || num == '') {
+                num = Math.floor(Math.random() * 15)
+            }
+            return num
+        }  
+        numTwo = chooseNumTwo() 
+
+        const chooseNumThree = () => {
+            let num = '';
+            while (num == numOne || num == numTwo || num == '') {
+                num = Math.floor(Math.random() * 15)
+            }
+            return num
+        }  
+        numThree = chooseNumThree() 
+
+        return [numOne, numTwo, numThree]
+    }
+    const randomizeBoard = () => {
+        const numbers = chooseThreeNums()
+        console.log(numbers)
+
+        idOne = numbers[0]
+        srcOne = dogs[numbers[0]].source
+        idTwo = numbers[1]
+        srcTwo = dogs[numbers[1]].source
+        idThree = numbers[2]
+        srcThree = dogs[numbers[2]].source
+    }
+
+    randomizeBoard()
+    const gameContents = (
+        <div id='gameContents'>
+            <div id={idOne} className='card'>
+                <img className='card-pic' src={srcOne}></img>
+            </div>
+            <div id={idTwo} className='card'>
+                <img className='card-pic' src={srcTwo}></img>
+            </div>
+            <div id={idThree} className='card'>
+                <img className='card-pic' src={srcThree}></img>
+            </div>
+        </div>
+    )
+
+    return (
+        <div id='game'>
+            {gameContents}
+        </div>
+    )
 }
+
+export default Game
