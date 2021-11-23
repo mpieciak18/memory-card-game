ade44c199650777134fe264581e58a9681011199
Mark Pieciak <mpieciak18@msn.com>
2021-11-23 15:44:58 -0500

    add basic styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import '../stylesheets/Game.css'
 
 const Game = (props) => {
     const { dogs, setDogs, setScore, setGameOver } = props
