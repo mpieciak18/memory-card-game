2f86677cce0ee895952e2e40a91a212c81bbd281
Mark Pieciak <mpieciak18@msn.com>
2021-11-29 08:18:33 -0500

    update href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,10 @@ import githubLogo from '../images/github-logo.png'
 const Footer = () => {
     return (
         <div id='footer'>
-            <p>Developed by Mark Pieciak</p>
-            <img src={githubLogo} id='github-logo' href='https://github.com/mpieciak18/memory-card-game'></img>
+            <p id='footer-text'>Developed by Mark Pieciak</p>
+            <a href='https://github.com/mpieciak18/memory-card-game'>
+                <img src={githubLogo} id='github-logo' />
+            </a>
         </div>
     )
 }
